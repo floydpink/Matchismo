a6ad00ffc9fab362fba5e76fd369c1838c8c6def
Floyd Pink <imbleedingme@googlemail.com>
2014-03-17 18:35:10 -0400

    assignment 1 is done - shows randomly a deck of playing cards

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,11 +7,44 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic)int flipCount;
+@property (strong, nonatomic)Deck *deck;
 
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck {
+    if (!_deck)_deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount changed to %d", self.flipCount);
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
+    } else {
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+            self.flipCount++;
+        }
+    }
+}
 
 @end
